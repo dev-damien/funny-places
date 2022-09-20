4bc16e5907c34c678c2304450377599f11761813
Damien Völker <66248616+dev-damien@users.noreply.github.com>
2022-09-20 23:09:11 +0200

    reset number 2

--- frontend/app/src/androidTest/java/de/damien/frontend/ExampleInstrumentedTest.kt
+++ frontend/app/src/androidTest/java/de/damien/frontend/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("de.damien.frontend", appContext.packageName)
+        assertEquals("com.plcoding.cryptocurrencyappyt", appContext.packageName)
     }
 }
--- frontend/app/src/main/java/de/damien/frontend/MainActivity.kt
+++ frontend/app/src/main/java/de/damien/frontend/MainActivity.kt
@@ -1,11 +1,21 @@
 package de.damien.frontend
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import de.damien.frontend.ui.theme.FunnyPlacesTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            FunnyPlacesTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
     }
 }

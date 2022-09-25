326442a3c89abfebe9e505568fed965cc0b7ee29
Damien Völker <66248616+dev-damien@users.noreply.github.com>
2022-09-25 17:57:05 +0200

    you can now add comments to places

--- frontendV2/app/src/main/java/de/damien/frontend/PlaceDetailActivity.kt
+++ frontendV2/app/src/main/java/de/damien/frontend/PlaceDetailActivity.kt
@@ -1,14 +1,23 @@
 package de.damien.frontend
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.InputType
 import android.text.method.MovementMethod
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.AuthFailureError
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.bumptech.glide.Glide
 import de.damien.frontend.recyclerviews.comment.Comment
 import de.damien.frontend.recyclerviews.comment.CommentAdapter
@@ -28,15 +37,28 @@ class PlaceDetailActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        tvAddPlaceDescContent.movementMethod = ScrollingMovementMethod()
+        placeId = intent.getStringExtra("placeId")!!
+
+        tvPlaceDetailDescContent.movementMethod = ScrollingMovementMethod()
 
         rvPlaceComments.adapter = adapter
         rvPlaceComments.layoutManager = LinearLayoutManager(this)
 
-        placeId = intent.getStringExtra("placeId")!!
+        buPlaceDetailAddComment.setOnClickListener {
+            showdialog()
+        }
 
         getPlace()
-        getComments()
+
+        //pulling for new comments
+        val mainHandler = Handler(Looper.getMainLooper())
+        mainHandler.post(object : Runnable {
+            override fun run() {
+                getComments()
+                mainHandler.postDelayed(this, Constants.PULL_DELAY)
+            }
+        })
+
     }
 
     private fun getPlace() {
@@ -56,9 +78,9 @@ class PlaceDetailActivity : AppCompatActivity() {
                     image = response.getJSONObject("image")
                     val url = "${Constants.SERVER_URL}/images/${image.get("imageId")}"
 
-                    Glide.with(this).load(url).into(ivAddPlaceImage)
-                    tvAddPlaceTitleContent.text = response.get("title").toString()
-                    tvAddPlaceDescContent.text = response.get("description").toString()
+                    Glide.with(this).load(url).into(ivPlaceDetailImage)
+                    tvPlaceDetailTitleContent.text = response.get("title").toString()
+                    tvPlaceDetailDescContent.text = response.get("description").toString()
                     tvPlaceCreatorContent.text = creator.get("name").toString()
                     tvPlaceLatValue.text = response.get("latitude").toString()
                     tvPlaceLonValue.text = response.get("longitude").toString()
@@ -83,6 +105,7 @@ class PlaceDetailActivity : AppCompatActivity() {
                 url,
                 null,
                 Response.Listener { response ->
+                    commentList.clear()
                     Log.i(Constants.TAG, "API call to get all comments by placeId: $response")
                     for (i in 0 until response.length()) {
                         curComment = response.getJSONObject(i)
@@ -105,5 +128,64 @@ class PlaceDetailActivity : AppCompatActivity() {
         } catch (ex: Exception) {
 
         }
+    }
+
+    fun showdialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Title")
+        val input = EditText(this)
+        input.hint = "Enter Comment"
+        //input.inputType = InputType.TYPE_CLASS_TEXT
+        input.isSingleLine = false
+        input.inputType = InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE
+        builder.setView(input)
+
+        builder.setPositiveButton("OK") { _, _ ->
+            val textInput = input.text.toString()
+            if (textInput.isBlank()) {
+                Toast.makeText(this, "You have to enter something", Toast.LENGTH_SHORT).show()
+            } else{
+                Toast.makeText(this, "Comment has been added", Toast.LENGTH_SHORT).show()
+                addComment(textInput)
+            }
+        }
+        builder.setNegativeButton("Cancel") { dialog, which ->
+            dialog.cancel()
+        }
+        builder.show()
+    }
+
+    private fun addComment(text: String) {
+        Log.i(Constants.TAG, "try to add comment: $text")
+        val url = Constants.SERVER_URL + "/comments"
+        val request = object : StringRequest(
+            Method.POST,
+            url,
+            Response.Listener { response ->
+                Log.i(Constants.TAG, "POST /comments response: $response")
+            }, Response.ErrorListener { error ->
+                Log.e(Constants.TAG, "Something failed: $error")
+            }) {
+            override fun getBodyContentType(): String {
+                return "application/json"
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getBody(): ByteArray {
+                val params = HashMap<String, String>()
+                params["text"] = text
+                val jsonRequestBody = JSONObject(params as Map<*, *>?)
+
+                val creatorJson = JSONObject("{name: ${SessionData.name}}")
+                jsonRequestBody.put("writer", creatorJson)
+
+                val placeJson = JSONObject("{placeId: $placeId}")
+                jsonRequestBody.put("place", placeJson)
+
+                Log.i(Constants.TAG, "POST /comments JSON: $jsonRequestBody")
+                return jsonRequestBody.toString().toByteArray()
+            }
+        }
+        VolleySingleton.getInstance(this).addToRequestQueue(request)
     }
 }
